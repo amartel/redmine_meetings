29886c7b38087cb3b2a7732b2a6c92dbb12e9235
Arnaud MARTEL <arnaud@aclnr.net>
2012-02-06 18:29:54 +0100

    Fixed: compatibility with redmine 1.3.0 stable

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 #map.connect ':controller/:action/:id'
 ActionController::Routing::Routes.draw do |map|
   map.connect 'projects/:project_id/meetings/:action', :controller => 'meetings'
-  map.connect 'meetings/:id/:action', :controller => 'meetings'
+  map.connect 'meetings/:action/:id', :controller => 'meetings'
   map.connect 'projects/:id/meetings_settings/:action', :controller => 'meetings_settings'
 end
