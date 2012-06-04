8cbde58d4592869fc1fefd7c8e4d3192486ac37b
Arnaud MARTEL <arnaud@aclnr.net>
2012-06-04 09:42:31 +0200

    Compatibility with redmine 1.4.2

--- init.rb
+++ init.rb
@@ -24,7 +24,7 @@ Redmine::Plugin.register :redmine_meetings do
   name 'Meetings plugin'
   author 'Arnaud Martel'
   description 'plugin to manage meetings in REDMINE'
-  version '0.1.1'
+  version '0.1.2'
   requires_redmine :version_or_higher => '1.3.0'
   
   settings :default => {'bbb_server' => '', 'bbb_salt' => '', 'bbb_timeout' => '3', 'meeting_timezone' => 'Paris'}, :partial => 'meetings_settings/settings'
