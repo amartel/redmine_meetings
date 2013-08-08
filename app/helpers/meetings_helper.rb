06f301122e2ef26d42aeb54c88e8c9e58195ff8e
alexgotardi <alexgotardi@gmail.com>
2013-08-08 16:31:55 -0700

    Convertendo data

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -150,7 +150,7 @@ module MeetingsHelper
         else
           meeting_tz = User.current.time_zone ? User.current.time_zone : ActiveSupport::TimeZone[Setting.plugin_redmine_meetings['meeting_timezone']]
           docRecord.root.elements['recordings'].each do |recording|
-            dataFormated = meeting_tz.utc_to_local(recording.elements['startTime'].to_time).strftime("%F %R") 
+            dataFormated = meeting_tz.utc_to_local(recording.elements['startTime'].text.to_time).strftime("%F %R") 
             output << ("&nbsp;&nbsp;- <a href='#{server}/playback/slides/playback.html?meetingId=" + recording.elements['recordID'].text + "' target='" + (Setting.plugin_redmine_meetings['bbb_popup'] != '1' ? '_self' : '_blank') + "'>"+ dataFormated + "</a><br>").html_safe
           end
         end
