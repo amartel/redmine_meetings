02b4d618c1981c0b5d890df9cb74027944da18d4
Rumen Nikiforov <unafraid89@gmail.com>
2018-01-09 17:14:20 +0200

    Fixing  ruby 4 errors
    Fixing errors like "should not use the `match` method in your router without specifying an HTTP method."

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ RedmineApp::Application.routes.draw do
 #  map.connect 'projects/:project_id/meetings/:action', :controller => 'meetings'
 #  map.connect 'meetings/:action/:id', :controller => 'meetings'
 #  map.connect 'projects/:id/meetings_settings/:action', :controller => 'meetings_settings'
-  match 'projects/:project_id/meetings/:action', :controller => 'meetings'
-  match 'meetings/:action/:id', :controller => 'meetings'
-  match 'projects/:id/meetings_settings/:action', :controller => 'meetings_settings'
+  match 'projects/:project_id/meetings/:action', :via => [:get], :controller => 'meetings'
+  match 'meetings/:action/:id', :via => [:get], :controller => 'meetings'
+  match 'projects/:id/meetings_settings/:action', :via => [:get], :controller => 'meetings_settings'
 
 end
